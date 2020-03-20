31d2fd30b9ab2db7b7a35a75cd62c3b5b09ec5db
Jonas Johansson <jan.jonas.johansson@gmail.com>
2020-03-21 00:11:10 +0100

    t

--- react-frontend/src/views/start/start.js
+++ react-frontend/src/views/start/start.js
@@ -1,45 +1,74 @@
-import React, { useContext, useEffect, useState, useCallback } from 'react'
+import React, { useContext, useEffect, useState, useCallback } from "react";
 import { useHistory } from "react-router-dom";
-import { Socket, GameState } from '../../contexts';
-import './start.scss'
-import { SocketContext } from '../../sockets/socketProvider';
+import { Socket, GameState } from "../../contexts";
+import { SocketContext } from "../../sockets/socketProvider";
+import s from "./start.module.scss";
 
-const Start = (props) => {
-  console.log(props)
-  const {socket} = useContext(SocketContext)
-  const {gameState} = useContext(GameState)
-  const [gameId, setGameId] = useState('');
+const Start = props => {
+  console.log(props);
+  const { socket } = useContext(SocketContext);
+  const { gameState } = useContext(GameState);
+  const [gameId, setGameId] = useState("");
+  const [username, setUsername] = useState("");
   const history = useHistory();
 
-  const goTo = useCallback((path) => history.push(path), [history])
+  const goTo = useCallback(path => history.push(path), [history]);
 
   useEffect(() => {
-    if (gameState && sessionStorage.getItem('playerId')) return goTo(`game/${gameState.id}/${sessionStorage.getItem('playerId')}`)
-    if (gameState) return goTo(`game/${gameState.id}`)
-  }, [gameState, goTo])
+    if (gameState && sessionStorage.getItem("playerId"))
+      return goTo(`game/${gameState.id}/${sessionStorage.getItem("playerId")}`);
+    if (gameState) return goTo(`game/${gameState.id}`);
+  }, [gameState, goTo]);
 
-  const connectToGame = (e) => {
+  const connectToGame = e => {
     e.preventDefault();
-    socket.send(JSON.stringify({action: "JOIN_GAME", id: gameId}));
-  }
+    socket.send(JSON.stringify({ action: "JOIN_GAME", id: gameId }));
+  };
 
-  const handleInput = (e) => setGameId(e.target.value);
+  const handleInput = setFn => e => setFn(e.target.value);
 
-  return (<>
-    <div>
-      <p>{}</p>
-      Or Join a game <br/>
-      <form id="joinRoomForm" onSubmit={connectToGame}>
-        <input type="text" onChange={handleInput} value={gameId} autoFocus></input>
-      </form>
-      <br/>
-      <button type={'submit'} form={'joinRoomForm'}>
-        Join game
-      </button>
-    </div>
-    <button onClick={() => socket.send(JSON.stringify({action: "GET_GAME_STATE"}))}>GET_GAME_STATE</button>
-    <button onClick={() => socket.send(JSON.stringify({action: "CREATE_GAME", id: null}))} disabled={!socket}>CREATE_GAME</button>
-  </>);
-}
+  return (
+    <>
+      <div className={s["root"]}>
+        <h1>Welcome</h1>
+        <button
+          onClick={() =>
+            socket.send(JSON.stringify({ action: "CREATE_GAME", id: null }))
+          }
+          disabled={!socket}
+        >
+          Create Game
+        </button>
+        <h2>Or Join a game</h2>
+        <form id="joinRoomForm" onSubmit={connectToGame}>
+          <label className={s["input-label"]} htmlFor="room-code">
+            Enter room code
+            <input
+              type="text"
+              name="room-code"
+              onChange={handleInput(setGameId)}
+              value={gameId}
+              autoFocus
+            ></input>
+          </label>
+          <label className={s["input-label"]} htmlFor="username">
+            Username
+            <input
+              type="text"
+              name="username"
+              onChange={handleInput(setUsername)}
+              value={username}
+              autoFocus
+            ></input>
+          </label>
+        </form>
+        <br />
+        <button type={"submit"} form={"joinRoomForm"}>
+          Join game
+        </button>
+      </div>
+    </>
+  );
+};
 
 export default Start;
